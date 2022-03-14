3ff608f91f98f7506525645c5031da3c097f6073
abbas <payami2013@gmail.com>
2022-03-14 16:42:54 +0400

    all file

--- src/main/java/com/example/demo/DemoRepo.java
+++ src/main/java/com/example/demo/DemoRepo.java
@@ -1,6 +1,6 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemoRepo extends JpaRepository {
-}
+//package com.example.demo;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface DemoRepo extends JpaRepository {
+//}
